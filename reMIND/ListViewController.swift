8a8feb3a84a9144cbddba524b45ebb07dce59bf9
Eugenio Barquin <eu.barquin@me.com>
2016-12-30 09:24:07 +0100

    Definimos los dos métodos para nuestra tableView. Realizamos la lógica para el botón agregar nueva tarea

--- reMIND/ListViewController.swift
+++ reMIND/ListViewController.swift
@@ -10,9 +10,56 @@ import UIKit
 
 class ListViewController: UITableViewController {
     
-
+    var taskManager = TaskManager.shareInstance
+    
     @IBAction func addTask(_ sender: UIBarButtonItem) {
+       
+        let alertController = UIAlertController(title: "Nueva tarea", message: "Nombre de la tarea", preferredStyle: .alert)
         
+        alertController.addTextField(configurationHandler: nil)
         
+        let alerAction = UIAlertAction(title: "Guardar", style: .default) { (alertAction) in
+            
+            if let textFieldText = alertController.textFields?.first?.text {
+                self.taskManager.tasks.append(["title": textFieldText])
+                self.tableView.reloadData()
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        
+        alertController.addAction(alerAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    //Indicamos el numero de celdas por seccion
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskManager.tasks.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellIdentifier", for: indexPath) as!TaskCell
+        
+        let task = taskManager.tasks[indexPath.row]
+        
+        cell.lblTask.text = task["title"]
+        
+        if let image = task["icon"] {
+            cell.imgTask.image = UIImage(named: image)
+        } else {
+            cell.imgTask.image = UIImage(named: "img_no_icon")
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 80.0
     }
 }
